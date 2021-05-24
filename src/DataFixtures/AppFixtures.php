4b1b837bc12fb37428d860f63148977faa2b873a
guillaume-goubel <guillaume.goubel.pro@gmail.com>
2021-05-24 18:12:54 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Article;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        $this->encoder = $encoder;
+    }
+    
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($i=0; $i < 10 ; $i++) { 
+            
+            $user = new User();
+            $hashPass = $this->encoder->encodePassword($user ,'password');
+            $user->setEmail($faker->email)
+                 ->setPassword($hashPass) 
+            ;
+
+            $manager->persist($user);
+
+            for ($a=0; $a < random_int(5,15) ; $a++) { 
+                $article = (new Article())->setAuthor($user)
+                ->setContent($faker->text(300))
+                ->setName($faker->text(50));
+
+                $manager->persist($article);
+            }
+            
+        }
 
         $manager->flush();
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
